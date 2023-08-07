2edba9efeff83f72793558559f50f680772a9231
Samit Madhesiya <97397076+samit08@users.noreply.github.com>
2023-08-07 22:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (24.76%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& I) {
+         string sol=s;
+        for(int i = 0 ; i< I.size();i++){
+            sol[I[i]] = s[i];
+        }
+
+        return sol;
+    }
+};
