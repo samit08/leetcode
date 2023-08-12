f7175d234eae767703d5543b07a9241cd64a4cad
Samit Madhesiya <97397076+samit08@users.noreply.github.com>
2023-08-12 15:47:07 +0530

    Time: 14 ms (60.07%) | Memory: 5.9 MB (88.84%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,40 @@
+#include <string>
+
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'I') {
+                if (i + 1 < s.length() && (s[i + 1] == 'V' || s[i + 1] == 'X')) {
+                    total -= 1;
+                } else {
+                    total += 1;
+                }
+            } else if (s[i] == 'X') {
+                if (i + 1 < s.length() && (s[i + 1] == 'L' || s[i + 1] == 'C')) {
+                    total -= 10;
+                } else {
+                    total += 10;
+                }
+            } else if (s[i] == 'C') {
+                if (i + 1 < s.length() && (s[i + 1] == 'D' || s[i + 1] == 'M')) {
+                    total -= 100;
+                } else {
+                    total += 100;
+                }
+            } else if (s[i] == 'V') {
+                total += 5;
+            } else if (s[i] == 'L') {
+                total += 50;
+            } else if (s[i] == 'D') {
+                total += 500;
+            } else if (s[i] == 'M') {
+                total += 1000;
+            }
+        }
+
+        return total;
+    }
+};
