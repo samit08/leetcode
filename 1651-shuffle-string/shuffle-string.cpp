1e1d90ea5070b47200662e2dafa1a23567b86f97
Samit Madhesiya <97397076+samit08@users.noreply.github.com>
2024-01-11 17:42:26 +0530

    Time: 4 ms (83.88%) | Memory: 15.7 MB (28.66%) - LeetSync

--- 1651-shuffle-string/shuffle-string.cpp
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -3,7 +3,7 @@ public:
     string restoreString(string s, vector<int>& I) {
          string sol=s;
         for(int i = 0 ; i< I.size();i++){
-            sol[I[i]] = s[i];
+            sol[I[i]]=s[i];
         }
 
         return sol;
