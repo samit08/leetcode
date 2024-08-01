f9aa27f4fc776b3094eb918c016f6e747119e041
Samit Madhesiya <97397076+samit08@users.noreply.github.com>
2024-08-01 22:09:17 +0530

    Time: 706 ms (43.46%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,17 +1,17 @@
 # Write your MySQL query statement below
--- select a.product_name,sum(b.unit) as unit from  Products as a right  join Orders as b on a.product_id=b.product_id where order_date >'2020-02-01' and order_date < '2020-02-29' group by b.product_id having unit >=100;
+select a.product_name,sum(b.unit) as unit from  Products as a right  join Orders as b on a.product_id=b.product_id where b.order_date >='2020-02-01' and b.order_date <= '2020-02-29' group by b.product_id having unit >=100;
 
 
 -- Write your MySQL query statement below
-SELECT p.product_name, 
-       SUM(o.unit) AS unit 
-FROM Products p 
-JOIN Orders o 
-ON p.product_id = o.product_id 
-WHERE o.order_date >= '2020-02-01' 
-  AND o.order_date <= '2020-02-29'
-GROUP BY o.product_id 
-HAVING unit >= 100;
+-- SELECT p.product_name, 
+--        SUM(o.unit) AS unit 
+-- FROM Products p 
+-- JOIN Orders o 
+-- ON p.product_id = o.product_id 
+-- WHERE o.order_date >= '2020-02-01' 
+--   AND o.order_date <= '2020-02-29'
+-- GROUP BY o.product_id 
+-- HAVING unit >= 100;
 
 
 
